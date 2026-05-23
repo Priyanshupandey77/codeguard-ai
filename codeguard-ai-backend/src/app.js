58487571e125789e0160de232836a04113993b54
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-05-23 08:24:16 +0530

    added protected Routes Middleware

--- codeguard-ai-backend/src/app.js
+++ codeguard-ai-backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
     res.json({
