91477eecada6918ec053ce9f3f4805a44dca2d65
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-05-29 00:41:10 +0530

    feat: added modern register UI

--- codeguard-ai-frontend/src/pages/Register.jsx
+++ codeguard-ai-frontend/src/pages/Register.jsx
@@ -1,5 +1,87 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import api from "../services/api";
+
 const Register = () => {
-  return <div>Register Page</div>;
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+
+      await api.post("/auth/register", formData);
+
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+      alert("Registration failed");
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black px-4">
+      <div className="w-full max-w-md bg-zinc-900 border border-zinc-800 rounded-2xl p-8 shadow-2xl">
+        <h1 className="text-3xl font-bold mb-2 text-center">Create Account</h1>
+
+        <p className="text-zinc-400 text-center mb-8">
+          Start reviewing code with AI
+        </p>
+
+        <form onSubmit={handleSubmit} className="space-y-5">
+          <input
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            className="w-full bg-zinc-800 border border-zinc-700 rounded-lg px-4 py-3 outline-none focus:border-blue-500"
+            onChange={handleChange}
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="w-full bg-zinc-800 border border-zinc-700 rounded-lg px-4 py-3 outline-none focus:border-blue-500"
+            onChange={handleChange}
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="w-full bg-zinc-800 border border-zinc-700 rounded-lg px-4 py-3 outline-none focus:border-blue-500"
+            onChange={handleChange}
+          />
+
+          <button>{loading ? "Creating..." : "Create Account"}</button>
+        </form>
+
+        <p className="text-zinc-400 text-center mt-6">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-500">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
