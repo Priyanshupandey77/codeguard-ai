3e1f2b2a812d40aa9650cab6d8ff31104864c455
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-05-29 00:04:47 +0530

    feat: added pages for Login Register Dashboard Review History

--- codeguard-ai-frontend/src/App.jsx
+++ codeguard-ai-frontend/src/App.jsx
@@ -1,8 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import History from "./pages/History";
+
 function App() {
   return (
-    <>
-      <h1>Priyanshu Pandey</h1>
-    </>
+    <div className="min-h-screen bg-gray-950 text-white">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </div>
   );
 }
 
